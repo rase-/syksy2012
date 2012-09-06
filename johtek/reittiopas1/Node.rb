cd48a01878db6b87e263917cf809457b3a74d8e0
Tony Kovanen <tonykovanen@hotmail.com>
2012-09-06 21:54:25 +0300

    initial commit

---
+++ johtek/reittiopas1/Node.rb
@@ -0,0 +1,28 @@
+class Node
+  attr_accessor :id, :name, :address, :x, :y, :neighbours, :colour
+
+  def hash_initialize(hash)
+    @id = hash["koodi"]
+    @name = hash["nimi"]
+    @address = hash["osoite"]
+    @x = hash["x"]
+    @y = hash["y"]
+    @neighbours = hash["naapurit"]
+    @colour = :white
+    return self
+  end
+
+  def initialize(id=nil, name=nil, address=nil, x=nil, y=nil, neighbours=nil)
+    @id = id
+    @name = name
+    @address = address
+    @x = x
+    @y = y
+    @neighbours = neighbours
+    @colour = :white
+  end
+
+  def to_s
+    @name
+  end
+end
