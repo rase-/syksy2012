9b445fd0ad5c5ea1d52a67f2a2ede2c5c9c2646f
Tony Kovanen <tonykovanen@hotmail.com>
2012-09-07 20:55:38 +0300

    Johtek ex 4 and reorganization of repository

---
+++ johtek/viikko1/reittiopas1/Node.rb
@@ -0,0 +1,26 @@
+class Node
+  attr_accessor :id, :name, :address, :x, :y, :neighbours
+
+  def hash_initialize(hash)
+    @id = hash["koodi"]
+    @name = hash["nimi"]
+    @address = hash["osoite"]
+    @x = hash["x"]
+    @y = hash["y"]
+    @neighbours = hash["naapurit"]
+    return self
+  end
+
+  def initialize(id=nil, name=nil, address=nil, x=nil, y=nil, neighbours=nil)
+    @id = id
+    @name = name
+    @address = address
+    @x = x
+    @y = y
+    @neighbours = neighbours
+  end
+
+  def to_s
+    @name
+  end
+end
