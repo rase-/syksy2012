ef4351aa84fea8aeef35aeed00418ee43286587e
Tony Kovanen <tonykovanen@hotmail.com>
2012-09-27 16:34:19 +0300

    New stuff to johtek

--- johtek/viikko3/chess_package/YourEvaluator.java
+++ johtek/viikko3/chess_package/YourEvaluator.java
@@ -1,24 +1,65 @@
 
+import java.util.HashMap;
+
 public class YourEvaluator extends Evaluator {
-	public double eval(Position p) {
-		double ret = 0;
-		for(int x = 0; x < p.board.length; ++x) {
-			for(int y = 0; y < p.board[x].length; ++y) {
-				if(p.board[x][y] == Position.Empty) continue;
-				if(p.board[x][y] == Position.WKing) ret += 1e9;
-				if(p.board[x][y] == Position.WQueen) ret += 9;
-				if(p.board[x][y] == Position.WRook) ret += 5.25;
-				if(p.board[x][y] == Position.WBishop) ret += 3.25;
-				if(p.board[x][y] == Position.WKnight) ret += 3;
-				if(p.board[x][y] == Position.WPawn) ret += 1;
-				if(p.board[x][y] == Position.BKing) ret -= 1e9;
-				if(p.board[x][y] == Position.BQueen) ret -= 9;
-				if(p.board[x][y] == Position.BRook) ret -= 5.25;
-				if(p.board[x][y] == Position.BBishop) ret -= 3.25;
-				if(p.board[x][y] == Position.BKnight) ret -= 3;
-				if(p.board[x][y] == Position.BPawn) ret -= 1;
-			}
-		}
-		return ret;
-	}
+  
+  
+  
+  public double materialBalance(Position p) {
+    // Loop doing simplified material balance
+    for (int x = 0; x < p.board.length; ++x) {
+      for (int y = 0; y < p.board[x].length; ++y) {
+        if (p.board[x][y] == Position.Empty) {
+          continue;
+        }
+        if (p.board[x][y] == Position.WKing) {
+          ret += 32767;
+        }
+        if (p.board[x][y] == Position.WQueen) {
+          ret += 975;
+        }
+        if (p.board[x][y] == Position.WRook) {
+          ret += 500;
+        }
+        if (p.board[x][y] == Position.WBishop) {
+          ret += 325;
+        }
+        if (p.board[x][y] == Position.WKnight) {
+          ret += 320;
+        }
+        if (p.board[x][y] == Position.WPawn) {
+          ret += 100;
+        }
+        if (p.board[x][y] == Position.BKing) {
+          ret -= 32767;
+        }
+        if (p.board[x][y] == Position.BQueen) {
+          ret -= 975;
+        }
+        if (p.board[x][y] == Position.BRook) {
+          ret -= 500;
+        }
+        if (p.board[x][y] == Position.BBishop) {
+          ret -= 325;
+        }
+        if (p.board[x][y] == Position.BKnight) {
+          ret -= 320;
+        }
+        if (p.board[x][y] == Position.BPawn) {
+          ret -= 100;
+        }
+        
+      }
+    }
+    return ret;
+  }
+  
+  public double eval(Position p) {
+    double ret = 0;
+    
+    ret += materialBalance(p);
+    
+    
+    return ret;
+  }
 }
--- johtek/viikko3/generate_tuples.rb
+++ johtek/viikko3/generate_tuples.rb
@@ -1,6 +1,5 @@
 require 'pp'
 
-
 def random_number(distribution)
   r = rand # Random number in [0,1]
   return 1 if r > distribution[0]
@@ -31,7 +30,7 @@ end
 def pretty_print_tuples(tuples)
   tuples.each do |tuple|
     #(A,R,S,B,K,L)
-    puts "(" + tuple[:A].to_s + "," + tuple[:R].to_s + "," + tuple[:S].to_s + "," + tuple[:B].to_s + "," + tuple[:K].to_s + "," + tuple[:L].to_s + ")" 
+    puts "(" + tuple[:A].to_s + "," + tuple[:R].to_s + "," + tuple[:S].to_s + "," + tuple[:B].to_s + "," + tuple[:K].to_s + "," + tuple[:L].to_s + ")"
   end
 end
 
@@ -42,20 +41,20 @@ class BayesModel
     @nodes_by_id = {}
     @roots = []
   end
-  
+
   def add_node(node)
     @nodes << node
     @nodes_by_id[node.id] = node
     @roots << node if node.parents.empty?
   end
-  
+
   def find_node(id)
     return @nodes_by_id[id]
   end
-  
+
   class Node
     attr_accessor :parents, :children, :probability, :id
-    
+
     # Probability holds given that the value of all parents is 1
     def initialize(id, parents=[], children=[], probability=0)
       @id = id
@@ -63,7 +62,7 @@ class BayesModel
       @children = children
       @probability = probability
     end
-    
+
     def distribution
       return [1-@probability, @probability]
     end
@@ -86,7 +85,7 @@ not_l_and_not_b = 0
 
 not_k = 0
 not_k_and_a = 0
-not_k_and_b = 0
+not_k_and_a_and_b = 0
 
 tuples.each do |tuple|
   if tuple[:L] == 0
@@ -94,14 +93,16 @@ tuples.each do |tuple|
     not_l_and_not_a += 1 if tuple[:A] == 0
     not_l_and_not_b += 1 if tuple[:B] == 0
   end
-  
+
   if tuple[:K] == 0
     not_k += 1
-    not_k_and_a += 1 if tuple[:A] == 1
-    not_k_and_b += 1 if tuple[:B] == 1
+    if tuple[:A] == 1
+      not_k_and_a += 1 
+      not_k_and_a_and_b += 1 if tuple[:B] == 1
+    end
   end
 end
 puts "NOT L: " + not_l.to_s + " | NOT L AND NOT A: " + not_l_and_not_a.to_s + " | PERCENTAGE: " + (1.0 * not_l_and_not_a/not_l).to_s
 puts "NOT L: " + not_l.to_s + " | NOT L AND NOT B: " + not_l_and_not_b.to_s + " | PERCENTAGE: " + (1.0 * not_l_and_not_b/not_l).to_s
 puts "NOT K: " + not_k.to_s + " | NOT K AND A: " + not_k_and_a.to_s + " | PERCENTAGE: " + (1.0 * not_k_and_a/not_k).to_s
-puts "NOT K: " + not_k.to_s + " | NOT K AND B: " + not_k_and_b.to_s + " | PERCENTAGE: " + (1.0 * not_k_and_b/not_l).to_s
+puts "NOT K: " + not_k.to_s + " | NOT K AND A AND B: " + not_k_and_a_and_b.to_s + " | PERCENTAGE: " + (1.0 * not_k_and_a_and_b/not_l).to_s
